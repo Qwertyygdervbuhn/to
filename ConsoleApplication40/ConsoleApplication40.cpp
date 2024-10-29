e0d119f543f7261da8e802831cc870a55c187e46
Alexander Dm. <dabl21291@gmail.com>
2024-10-29 22:10:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication40/ConsoleApplication40.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+
+    Node(int val) : value(val), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void add(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    void print() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->value << " ";
+            current = current->next;
+
+        }
+
+    }
+
+
+    ~LinkedList() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* nextNode = current->next;
+            delete current;
+            current = nextNode;
+            cout << "List deleted" << endl;
+        }
+
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.add(10);
+    list.add(20);
+    list.add(30);
+
+    cout << "Elements in the list: ";
+    list.print();
+
+ 
+}
